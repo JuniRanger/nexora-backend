528cbcae37e24c9083e078f2b2d53079916c61c0
angelpalma01 <angelone237@gmail.com>
2026-04-16 14:07:25 -0600

    cors
    cambie el cors y como se referenciaba la contraseña

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import 'reflect-metadata';
+import 'dotenv/config'; // <-- Añade esto
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
--- src/modules/auth/infrastructure/services/bcrypt-password-hasher.service.ts
+++ src/modules/auth/infrastructure/services/bcrypt-password-hasher.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 import type { IPasswordHasher } from '../../application/interfaces/password-hasher.interface';
 
 @Injectable()
